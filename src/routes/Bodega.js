b2d103f6010d12d1da86e1fb385d2067cc38954d
antonio madureira <antonio.madureira2222@gmail.com>
2022-06-02 00:52:54 -0400

    changed style sheets. Had issues at first with formatting. Hav to clean up files

--- src/routes/Bodega.js
+++ src/routes/Bodega.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import "../Bodega.css";
 
 const Bodega = () => {
   return (
-    <div>Bodega</div>
+        <div className='img-container'>
+  
+            <img src='/images/bodega4.png' alt='bodega' />
+        </div>
   )
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import "../index.css";
+import "../Home.css";
 
 const Home = () => {
   return (
     <div>
        <div className='container'>
-       <img src='/images/store4.png' alt='store' />
+       <a href='/Bodega'> <img id="img1" src='/images/store4.png' alt='store'/></a>
        </div>
     </div>
   )
