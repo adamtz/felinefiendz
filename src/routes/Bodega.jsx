edda17e64a8593a7ac8c28369c14e3236b387e9c
antonio madureira <antonio.madureira2222@gmail.com>
2022-06-30 14:00:52 -0400

    setting up the route for the store

--- src/routes/Bodega.jsx
+++ src/routes/Bodega.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 //imported packages
-import VideoLooper from "react-video-looper"
+import VideoLooper from 'react-video-looper'
 import FiendzVideo from "../assets/Bodega Video2.mp4"
 
 const Bodega = () => {
   return (
-    <div className='btn2'>
+    <div className='main'>
       <div className='video2'>
-        <VideoLooper source={FiendzVideo} start={6.00} end={8.00} objectFit autoPlay />
+        <VideoLooper source={FiendzVideo} start={6.00} end={9.00} loopCount={10} autoPlay objectFit />
         </div>
     </div>
   )
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import "../components/Main.css"
 //imported packages
 //import VideoLooper from "react-video-looper"
@@ -13,9 +14,11 @@ const Main = () => {
         // Start of the animation loop from the mp4 file
         //<VideoLooper src={FiendzVideo} start={0.00} end={4.00} loop autoPlay /> 
         <div className='main'>
-           <video src= {FiendzVideo} autoPlay loop muted />
+           <video src= {FiendzVideo} autoPlay loop />
            <div className='content'>
-            <button>enter</button>
+            <Link to='/Bodega'>
+                <button>enter</button> 
+            </Link>
            </div>
            
         </div>
