47d7fa4e478965abf4704404597ced4b196ddecf
antonio madureira <antonio.madureira2222@gmail.com>
2022-06-30 15:36:17 -0400

    added the click feature to get in the store

---
+++ src/routes/Bodega.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//imported packages
+//import VideoLooper from "react-video-looper"
+import FiendzVideo from "../assets/Bodega Video2_instore.mp4"
+
+const Bodega = () => {
+  return (
+    <div className='btn2'>
+      <div className='video2'>
+        <video src={FiendzVideo} autoPlay  muted > </video>
+        </div>
+    </div>
+  )
+}
+
+export default Bodega
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//stylesheet
 import "../components/Main.css"
-import "../components/button.css"
+
 //imported packages
 //import VideoLooper from "react-video-looper"
 import FiendzVideo from "../assets/Bodega Video2_Trim.mp4"
+
 
 //song import
 //import song from "../assets/static/bodegamusic.mp3"
@@ -14,12 +18,10 @@ const Main = () => {
         // Start of the animation loop from the mp4 file
         //<VideoLooper src={FiendzVideo} start={0.00} end={4.00} loop autoPlay /> 
         <div className='main'>
-           <video src= {FiendzVideo} autoPlay loop muted />
-           <div className='content'>
-            <div class='container'>
-                <button className='btn1'>enter</button>
-            </div>
-           </div>
+            <Link to="/Bodega">
+           <video src= {FiendzVideo} autoPlay loop muted /> 
+           </Link>
+           
            
         </div>
 
