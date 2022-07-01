4e3fa89ed39a4467999bdbd70cc6ed7b17f1036b
antonio madureira <antonio.madureira2222@gmail.com>
2022-07-01 19:17:20 -0400

    added the button features and structure for links to go to appropriate destinations

--- src/routes/Bodega.jsx
+++ src/routes/Bodega.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-//import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 //stylesheet
 import "../components/bodega.css"
+import "../components/button.css"
 
 //imported packages
 //import VideoLooper from "react-video-looper"
@@ -15,6 +16,26 @@ const Bodega = () => {
         <a href='https://discord.com/channels/934616456015007815/953830951811686470'>
           <button>Click me</button>
           </a>
+        </div>
+        <div className='btn3'>
+          <Link to="/Merch">
+          <button>play</button>
+          </Link>
+        </div>
+        <div className='btn4'>
+          <Link to="/RoadMap">
+          <button>roadmap</button>
+          </Link>
+        </div>
+        <div className='btn5'>
+          <a href='https://opensea.io/collection/felinefiendznft'>
+          <button>opensea</button>
+          </a>
+        </div>
+        <div className='btn6'>
+          <Link to="/team">
+          <button>team</button>
+          </Link>
         </div>
       <div className='video2'>
         <video src={FiendzVideo2} autoPlay muted > </video>
