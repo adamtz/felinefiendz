7ba5ccfc99d8903056066983064b107654428f0b
antonio madureira <antonio.madureira2222@gmail.com>
2022-07-01 15:14:55 -0400

    added a final video animation for in store

--- src/routes/Bodega.jsx
+++ src/routes/Bodega.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
+//stylesheet
+import "../components/bodega.css"
+
 //imported packages
 //import VideoLooper from "react-video-looper"
 import FiendzVideo from "../assets/Bodega Video2_instore.mp4"
 
 const Bodega = () => {
-  return (
+  return ( 
     <div className='btn2'>
       <div className='video2'>
         <video src={FiendzVideo} autoPlay  muted > </video>
         </div>
     </div>
+    
   )
 }
+
 
 export default Bodega
