890ed7f8da2848d57edece824be6508d1a78a08d
antonio madureira <antonio.madureira2222@gmail.com>
2022-07-01 18:20:54 -0400

    added the button for discord click link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 //importing the use of route and routes for pages
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate} from "react-router-dom";
 
 
 import Main from "./routes/Main"
 import Merch from "./routes/Merch"
 import Bodega from "./routes/Bodega"
+import Transition from "./routes/Transition"
 import Team from "./routes/Team"
 import RoadMap from "./routes/RoadMap"
 
@@ -19,6 +20,8 @@ function App() {
        <Route path="/RoadMap" element={<RoadMap /> } />
        <Route path="/Team" element={<Team />} />
        <Route path="/Bodega" element={<Bodega />} />
+       <Route path="/Bodega2" element={<Navigate to="/Store"/>} />
+       <Route path="/Store" element={<Transition />} />
      </Routes>
     </div>
   );
--- src/routes/Bodega.jsx
+++ src/routes/Bodega.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
+//import Link from 'react-router-dom'
 
 //stylesheet
 import "../components/bodega.css"
 
 //imported packages
 //import VideoLooper from "react-video-looper"
-import FiendzVideo from "../assets/Bodega Video2_instore.mp4"
+import FiendzVideo2 from "../assets/Bodega Video2_instore.mp4"
 
 const Bodega = () => {
   return ( 
-    <div className='btn2'>
+    <div className='chief'>
+      <div className='btn2'>
+        <a href='https://discord.com/channels/934616456015007815/953830951811686470'>
+          <button>Click me</button>
+          </a>
+        </div>
       <div className='video2'>
-        <video src={FiendzVideo} autoPlay  muted > </video>
+        <video src={FiendzVideo2} autoPlay muted > </video>
         </div>
     </div>
     
   )
 }
+
+
 
 
 export default Bodega
