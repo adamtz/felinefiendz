dcc128fe8cde040ae987f8f102af4a3927b50e5e
antonio madureira <antonio.madureira2222@gmail.com>
2022-07-05 20:04:29 -0400

    added more text on roadmap page and more media query's

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 //importing the use of route and routes for pages
-import { Route, Routes, Navigate} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 
 
 import Main from "./routes/Main"
 import Merch from "./routes/Merch"
 import Bodega from "./routes/Bodega"
-import Transition from "./routes/Transition"
 import Team from "./routes/Team"
 import RoadMap from "./routes/RoadMap"
 
@@ -20,8 +19,6 @@ function App() {
        <Route path="/RoadMap" element={<RoadMap /> } />
        <Route path="/Team" element={<Team />} />
        <Route path="/Bodega" element={<Bodega />} />
-       <Route path="/Bodega2" element={<Navigate to="/Store"/>} />
-       <Route path="/Store" element={<Transition />} />
      </Routes>
     </div>
   );
--- src/routes/RoadMap.js
+++ src/routes/RoadMap.js
@@ -19,36 +19,47 @@ function RoadMap() {
         <ul>
           <li>January 27th, 2022 </li>
           <li>Discord opens to the public</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li> January 30th, 2022</li>
           <li>The release of our roadmap Season 1</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>February 9th, 2022</li>
           <li> Twitter reaches 10,000 fiendz + 17,000 members on discord</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>February 21st, 2022</li>
           <li>Website gets dropped</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>March 6th, 2022</li>
           <li>Public sale of Feline Fiendz with a rapid sell out in under 10 minutes</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>March 30th, 2022</li>
           <li>First Official fiendz meet up</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>April 6th, 2022</li>
           <li>Surpassed 2,000 eth Volume traded</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>April 10th, 2022</li>
           <li>Acquisition of our 1st Metaverse land</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>April 29th, 2022</li>
           <li>1st Airdrop to all Feline Fiendz holders</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>May 12th, 2022</li>
           <li>Feline Fiendz officially becomes verified on Open Sea</li>
+          <h4>COMPLETE</h4>
           <h1>|</h1>
           <li>May 16th, 2022</li>
           <li>Fiendz Bodega is open for business</li>
+          <h4>COMPLETE</h4>
         </ul>
       </div>
       <div>
